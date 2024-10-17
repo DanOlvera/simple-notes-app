291379b4ff2b3bb1646dedb4a352b33eaedea76a
Daniel Olvera <ing.josuedanielescamilla@gmail.com>
2024-10-17 15:25:55 -0700

    Completed room database setup for notes

---
+++ app/src/main/java/com/danielolvera/todonotes/model/local/NoteDao.kt
@@ -0,0 +1,26 @@
+package com.danielolvera.todonotes.model.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+// Data Access Object to create the definition of the CRUD operations
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes ORDER BY timestamp DESC")
+    fun getAllNotes(): Flow<List<Note>>
+}
